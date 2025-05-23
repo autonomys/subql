2ec4580ce4d2b9043ea0f2098aacd9bd26fcae40
xm0onh <saeedyazdinezhad@gmail.com>
2025-05-23 10:51:38 -0700

    Update Dockerfile to include modified node-core pkg and redis service

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,13 @@ apk add --no-cache jq
 npm install -g --force yarn@latest
 cd "$1"
 
+# Save the full locally-built node-core package for later
+if [ -d "../node-core/dist" ]; then
+  echo "Using local node-core package"
+  mkdir -p /tmp/node-core-backup
+  cp -r ../node-core/dist /tmp/node-core-backup/
+fi
+
 # Modifies the package.json to replace "workspace:*" versions with actual versions
 jq -r '.dependencies | to_entries[] | select(.value == "workspace:*") | .key' package.json | while read -r dep; do
   directory=$(jq --arg dep "$dep" -r '.compilerOptions.paths[$dep][0]' ../../tsconfig.json | cut -d'/' -f 2)
@@ -15,4 +22,6 @@ jq -r '.dependencies | to_entries[] | select(.value == "workspace:*") | .key' pa
 done
 
 yarn pack --filename app.tgz
+
+# Clean up
 rm -rf /root/.npm /root/.cache
